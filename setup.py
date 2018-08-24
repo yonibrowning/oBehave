2e4f35a09549e3eda480f4c4e3b18e3f481b049c
Yoni <yonib@uw.edu>
2018-08-23 20:22:58 -0700

    Modified to make package readable by PIP

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'swdb_2018_tools',
-    version = '0.1.0',
-    description = """shared tools package for SWDB 2018""",
-    author = "David Feng",
-    author_email = "davidf@alleninstitute.org",
-    url = 'https://github.com/AllenInstitute/allensdk.ipfx',
+    name = 'oBehave',
+    version = '0.2.0',
+    description = """Analysis Project for the Behavioral Analysis""",
+    author = "OBeh team",
+    author_email = "yonib@uw.edu",
+    url = 'https://github.com/yonibrowning/oBehave.git',
     packages = find_packages()
 )
