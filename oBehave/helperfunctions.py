6770fac5f2e0da54d132fe466dbe46f321e612c4
yonibrowning <yonib@uw.edu>
2018-08-23 21:14:45 -0700

    Update helperfunctions.py

--- oBehave/helperfunctions.py
+++ oBehave/helperfunctions.py
@@ -11,3 +11,4 @@ def load_manifest(drive_path = '/data/dynamic-brain-workshop/visual_behavior',
     drive_path (optional): Location of data on drive. Default is AWS location
     '''
     manifest = pd.read_csv(os.path.join(drive_path,manifest_file))
+    return manifest
