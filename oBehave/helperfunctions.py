50f18ba6a54e3cee00b1d3e8aec6ffda5c93967e
yonibrowning <yonib@uw.edu>
2018-08-25 22:02:13 +0000

    Forgot some stuff on last one

--- oBehave/helperfunctions.py
+++ oBehave/helperfunctions.py
@@ -7,8 +7,7 @@ import pandas as pd
 event_drive_path_AWS = '/data/dynamic-brain-workshop/visual_behavior_events' #AWS Location
 drive_path =  '/data/dynamic-brain-workshop/visual_behavior'
 
-def load_manifest(drive_path = drive_path,
-                  manifest_file = 'visual_behavior_data_manifest.csv'):
+def load_manifest(drive_path = drive_path,manifest_file = 'visual_behavior_data_manifest.csv'):
     '''
     This is just a rapper to load behavior dataset manifest. 
     Inputs 
@@ -19,14 +18,14 @@ def load_manifest(drive_path = drive_path,
 
 
 def load_dff_events_file(experiment_id,event_drive_path = event_drive_path_AWS):
-	'''
-	Loads a given behavior event file from a supplied experiment id
-	Can be used in conjunction with the manifest file, in a format
-	similar to the way dataset objs are called
-	Input
-	experiment_id: manifest ID
-	event_drive_path: Optional. Script has built in drive path to AWS location.
-	'''
+    '''
+    Loads a given behavior event file from a supplied experiment id
+    Can be used in conjunction with the manifest file, in a format
+    similar to the way dataset objs are called
+    Input
+    experiment_id: manifest ID
+    event_drive_path: Optional. Script has built in drive path to AWS location.
+    '''
     event_drive_path = '/data/dynamic-brain-workshop/visual_behavior_events'
     tmp = os.path.join(event_drive_path, str(experiment_id)+'_events.npz')
     tmp_data = np.load(tmp)
