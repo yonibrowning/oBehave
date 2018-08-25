a208ae225f460d3392f020e3dbdeb81def55b1ea
yonibrowning <yonib@uw.edu>
2018-08-25 16:36:37 +0000

    Added some plotting functions

--- oBehave/helperfunctions.py
+++ oBehave/helperfunctions.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 import pandas as pd
 
+# Local of the event drive path
+event_drive_path_AWS = '/data/dynamic-brain-workshop/visual_behavior_events' #AWS Location
 drive_path =  '/data/dynamic-brain-workshop/visual_behavior'
 
 def load_manifest(drive_path = drive_path,
@@ -14,3 +16,19 @@ def load_manifest(drive_path = drive_path,
     '''
     manifest = pd.read_csv(os.path.join(drive_path,manifest_file))
     return manifest
+
+
+def load_dff_events_file(experiment_id,event_drive_path = event_drive_path_AWS):
+	'''
+	Loads a given behavior event file from a supplied experiment id
+	Can be used in conjunction with the manifest file, in a format
+	similar to the way dataset objs are called
+	Input
+	experiment_id: manifest ID
+	event_drive_path: Optional. Script has built in drive path to AWS location.
+	'''
+    event_drive_path = '/data/dynamic-brain-workshop/visual_behavior_events'
+    tmp = os.path.join(event_drive_path, str(experiment_id)+'_events.npz')
+    tmp_data = np.load(tmp)
+    event_array = tmp_data['ev']
+    return event_array
